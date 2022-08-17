08f463972911901475d445327f9b00ee06fedbaa
OraniumTech1409 <oraniumtechvelachery@gmail.com>
2022-08-17 16:01:19 +0530

    Sec

--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -2,18 +2,32 @@ package test;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Parameters;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Test {
 
+	@Parameters({ "browser" })
 	@org.testng.annotations.Test
-	public void login() {
+	public void login(String browser) {
 
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.facebook.com");
-		driver.manage().window().maximize();
+		if (browser.equals("CHROME")) {
+
+			WebDriverManager.chromedriver().setup();
+			WebDriver driver = new ChromeDriver();
+			driver.get("https://www.facebook.com");
+			driver.manage().window().maximize();
+		}
+
+		else if (browser.equals("EDGE")) {
+			WebDriverManager.edgedriver().setup();
+			WebDriver driver = new EdgeDriver();
+			driver.get("https://www.facebook.com");
+			driver.manage().window().maximize();
+
+		}
 
 	}
 
